8547656b7d872aae4be13ce9811dae001ecfb129
Valentino Vizner <valentino.vizner@superbet.com>
2022-12-15 12:40:00 +0100

    feat: stage 1 port: 6379

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let mut listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     match listener.accept().await {
         Ok((_socket, addr)) => println!("new client: {:?}", addr),
